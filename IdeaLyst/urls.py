53f907e4d5d47a4e22e1d241317dce3da52823b8
Winnie Lee <weiyi_lee@berkeley.edu>
2017-10-07 16:45:20 -0700

    social login

--- IdeaLyst/urls.py
+++ IdeaLyst/urls.py
@@ -13,12 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home'
+
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 from . import views as core_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', core_views.index, name = 'home'),
-    url(r'^planner/', include('planner.urls', namespace='planner'))
+    url(r'^planner/', include('planner.urls', namespace='planner')),
+
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
--- IdeaLyst/views.py
+++ IdeaLyst/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home(request):
+    return render(request, 'core/home.html')
 
 def index(request):
     return render(request, 'core/index.html')
